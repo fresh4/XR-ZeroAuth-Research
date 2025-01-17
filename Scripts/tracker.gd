678f21a8e1fb4405238ad1ffd1554b946f92232e
Achkar <oachkar@CougarNet.UH.EDU>
2025-01-17 16:08:33 -0600

    record file save on move

--- Scripts/tracker.gd
+++ Scripts/tracker.gd
@@ -5,13 +5,17 @@ class_name Tracker extends Node3D
 # Default CSV column headers. Linear+angular velocity and position.
 const headers: String = "lv_x, lv_y, lv_z, av_x, av_y, av_z, pos_x, pos_y, pos_z\n";
 var data: String = headers;
+var idx: int = 1;
+var floor: float = .0001;
 
 func _ready() -> void:
 	Globals.recording_toggled.connect(_on_recording_toggle);
 
 func _physics_process(_delta: float) -> void:
-	if Globals.is_recording:
-		record_metrics();
+	if not Globals.is_recording: return
+	record_metrics();
+	if not is_moving():
+		save_data();
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("save_recording"):
@@ -20,9 +24,7 @@ func _input(event: InputEvent) -> void:
 # Every (physics) frame we append the current positional states as a row in the csv.
 # Only if the physics object is not stationary to reduce unnecessary row entries.
 func record_metrics():
-	var floor: float = .0001;
-	if (abs(rb.linear_velocity.x) > floor or abs(rb.linear_velocity.y) > floor or abs(rb.linear_velocity.z) > floor) or \
-	   (abs(rb.angular_velocity.x) > floor or abs(rb.angular_velocity.y) > floor or abs(rb.angular_velocity.z) > floor):
+	if is_moving():
 		print(rb.name + " " + str(rb.linear_velocity));
 		data += str(rb.linear_velocity.x) + ", " + str(rb.linear_velocity.y) + ", " + str(rb.linear_velocity.z) + ", "\
 			 + str(rb.angular_velocity.x) + ", " + str(rb.angular_velocity.y) + ", " + str(rb.angular_velocity.z) + ", "\
@@ -31,11 +33,16 @@ func record_metrics():
 # Takes the csv formatted string and writes to file as an actual csv. 
 # Happens for each physics object in the scene.
 func save_data():
-	var filename: String = str(rb.name) + ".csv";
+	var filename: String = str(rb.name) + "_" + str(idx) + ".csv";
 	var csv = FileAccess.open("user://" + filename, FileAccess.WRITE);
 	csv.store_string(data);
 	print("Saving " + filename + " in user directory. File size: " + str(csv.get_length()/1000.0) + "kb");
 	#OS.shell_show_in_file_manager(ProjectSettings.globalize_path("user://"));
+
+func is_moving() -> bool:
+	return (abs(rb.linear_velocity.x) > floor or abs(rb.linear_velocity.y) > floor or abs(rb.linear_velocity.z) > floor) or \
+	   (abs(rb.angular_velocity.x) > floor or abs(rb.angular_velocity.y) > floor or abs(rb.angular_velocity.z) > floor)
+		
 
 func _on_recording_toggle():
 	# If we start recording, reset the csv to record new information.
