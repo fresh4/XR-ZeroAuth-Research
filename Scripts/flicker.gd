d30fa62d6b249e7fdcb731c3518eaf3ed02ee4e6
fresh4 <fresh1736@gmail.com>
2025-07-07 17:24:05 -0500

    Create script to cause object to flicker

---
+++ Scripts/flicker.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var id: String = "";
+@export var frequency: int = 0;
+
+@onready var frame_rate = ProjectSettings.get_setting("physics/common/physics_ticks_per_second");
+
+var parent: Node3D;
+var mesh: MeshInstance3D;
+var time: float = 0;
+
+func _ready() -> void:
+	parent = get_parent();
+	mesh = parent.get_child(1);
+
+func _physics_process(delta: float) -> void:
+	time += delta;
+	var c = (127.5 + 127.5 * sin(2 * PI * frequency * time))/255;
+	var color = Color(c,c,c);
+	mesh.material_override.albedo_color = color;
