8c3fc1befa201580829379e64bf6e0ad96178690
fresh4 <fresh1736@gmail.com>
2025-07-07 20:35:48 -0500

    Can highlight object based on udp message received

--- Scripts/flicker.gd
+++ Scripts/flicker.gd
@@ -5,16 +5,25 @@ extends Node
 
 @onready var frame_rate = ProjectSettings.get_setting("physics/common/physics_ticks_per_second");
 
+const PORT = 6000;
+
 var parent: Node3D;
 var mesh: MeshInstance3D;
+var highlight_mesh: MeshInstance3D;
 var time: float = 0;
 
 func _ready() -> void:
+	Globals.msg_received.connect(highlight_object);
 	parent = get_parent();
 	mesh = parent.get_child(1);
+	highlight_mesh = parent.get_child(4);
+	highlight_mesh.visible = false;
 
 func _physics_process(delta: float) -> void:
 	time += delta;
 	var c = (127.5 + 127.5 * sin(2 * PI * frequency * time))/255;
 	var color = Color(c,c,c);
 	mesh.material_override.albedo_color = color;
+
+func highlight_object(msg) -> void:
+	highlight_mesh.visible = str(msg) == str(id);
--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -2,7 +2,9 @@ extends Node
 
 # A listenable event that fires whenever the recording state changes.
 signal recording_toggled; 
+signal msg_received(msg);
 
+var server = UDPServer.new();
 # Constants for socket communication
 const HOST: String = "127.0.0.1"
 const PORT: int = 6000
@@ -14,10 +16,15 @@ var socket: PacketPeerUDP = PacketPeerUDP.new()
 var flag = "";
 
 func _ready() -> void:
+	server.listen(PORT);
+
 	socket.connect_to_host(HOST, PORT)
 	if socket.is_socket_connected():
 		print("Established UDP connection to " + HOST + ":" + str(PORT))
 		print("NOTE: UDP is connectionless, so there's no guarantee the host is alive.")
+
+func _physics_process(_delta: float) -> void:
+	listen();
 
 func _exit_tree() -> void:
 	socket.close()
@@ -33,3 +40,13 @@ func _input(event: InputEvent) -> void:
 func send_socket_data(msg: String):
 	if not socket.is_socket_connected(): return
 	socket.put_packet(msg.to_ascii_buffer())
+
+func listen() -> void:
+	server.poll();
+	if server.is_connection_available():
+		var peer = Globals.server.take_connection();
+		var packet = peer.get_packet();
+		var msg = packet.get_string_from_utf8().replace("\n", "");
+		print("Accepted peer: %s:%s" % [peer.get_packet_ip(), peer.get_packet_port()]);
+		print("Received data: %s" % [packet.get_string_from_utf8()]);
+		msg_received.emit(msg);
