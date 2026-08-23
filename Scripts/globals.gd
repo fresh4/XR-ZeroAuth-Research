c8f9b31f6cad1c71941615c7008e08b54b465b75
fresh4 <fresh1736@gmail.com>
2026-08-23 01:22:02 -0500

    Controller support and new UDP signals for new Galea headset

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -7,7 +7,7 @@ signal msg_received(msg);
 var server = UDPServer.new();
 # Constants for socket communication
 const HOST: String = "127.0.0.1"
-const PORT: int = 6000
+const PORT: int = 12340
 
 # Global variable that keeps track of recording states between different scripts.
 var is_recording: bool = false; 
@@ -40,6 +40,29 @@ func _input(event: InputEvent) -> void:
 func send_socket_data(msg: String):
 	if not socket.is_socket_connected(): return
 	socket.put_packet(msg.to_ascii_buffer())
+	if msg.contains("DROP"):
+		send_galea_marker(1)
+	elif msg.contains("PICKUP"):
+		send_galea_marker(8)
+	else:
+		send_galea_marker(0)
+
+func send_galea_marker(marker_value: float) -> void:
+	if not socket.is_socket_connected():
+		print("Socket not active!")
+		return
+		
+	# Create a buffer pool allocating exactly 8 bytes (64-bits)
+	var buffer: PackedByteArray = PackedByteArray()
+	buffer.resize(8)
+	
+	buffer.encode_double(0, marker_value)
+	
+	# Reverse the array elements to convert Little-Endian to Network Byte Order (Big-Endian)
+	buffer.reverse()
+	
+	# Push raw network bytes into the streaming pipe
+	socket.put_packet(buffer)
 
 func listen() -> void:
 	server.poll();
